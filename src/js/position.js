a1a77e2e8162074919550c277c0e4f79f9b52ba4
Vadim Derman <thenoodly@gmail.com>
2015-07-15 02:22:48 -0700

    промежуточный комит 15.07

--- src/js/position.js
+++ src/js/position.js
@@ -8,34 +8,65 @@ var modulePosition = (function(){
 			newPosition = $this.data('position');
 
 		console.log(newPosition);
+
 		water.position({
 			my: newPosition,
 			at: newPosition,
 			of: result
 		});
+
+		_displayInInput ();
+
 	}
 
 	function _positioningStep() {
 		var
-			$this 		= $(this),
-			img			= $('.result__wrap-water'),
-			step 		= 1,
-			curentPosX	= img.css('left'),
-			curentPosY	= img.css('top'),
-			newPosX		= curentPosX + step,
-			newPosY		= curentPosY + step,
-			resultInput	= $this.closest('input');
+			$this			= $(this),
+			defAxis			= $this.parent(),
+			step			= 1,
+			img				= $('.result__wrap-water'),
+			topCoord		= img.css('top'),
+			leftCoord		= img.css('left'),
+			currentY		= parseInt(topCoord),
+			currentX		= parseInt(leftCoord);
 
-			console.log(curentPosX);
-			console.log(curentPosY);
+		console.log(topCoord);
+		console.log(leftCoord);
+		// console.log($this);
 
-		if ( $this.hasClass('axis__button_up')) {
-			img.css('left', newPosX + 'px'
-			);
-			resultInput.val(newPosX);
+		if ( defAxis.hasClass('axis__control-x')) {
+			console.log('выполняем спуск');
+			if ($this.hasClass('axis__button_up')) {
+				img.css({'left': currentY + step + 'px'});
+			} else {
+				img.css({'left': currentY - step + 'px'});
+			}
+
+		} else {
+			console.log('двигаем в сторону');
+			if ($this.hasClass('axis__button_up')) {
+				img.css({'top': currentY - step + 'px'});
+			} else {
+				img.css({'top': currentY + step + 'px'});
+			}
 		}
-
+		_displayInInput();
 	}
+
+	function _displayInInput () {
+		var
+			img			= $('.result__wrap-water'),
+			topCoord	= img.css('left'),
+			leftCoord	= img.css('top'),
+			inputX		= $('#control-x'),
+			inputY		= $('#control-y'),
+			currentX	= Math.floor(parseInt(topCoord)),
+			currentY	= Math.round(parseInt(leftCoord));
+
+		inputX.val(currentX);
+		inputY.val(Math.abs(currentY));
+	}
+
 	return {
 		init : function () {
 			this.setUpListeners();
