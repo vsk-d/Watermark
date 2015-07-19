4f485e3bdf3d09457e4ca8b0a4e15bc2c46f8b27
V_Kurochkin <darkinmail@rambler.ru>
2015-07-19 18:39:12 +0300

    add reset mode

---
+++ src/js/reset.js
@@ -0,0 +1,56 @@
+var resetModul = (function(){
+    'use strict';
+    function _resetAll () { 
+
+    var
+    	waterImg = ".result__img-water",
+    	waterDiv = ".result__wrap-water",
+    	sliderOp = "#slider-range-min"
+
+    	$(waterDiv).css('top', '0');
+    	$(waterDiv).css('left', '0');
+    	$(waterImg).css('opacity', '100');
+    	$(sliderOp).slider({
+      range: "min",
+      value: 100,
+      min: 1,
+      max: 100,
+      slide: function( event, ui ) {
+        var opacity = ui.value,
+                    _length = opacity.toString()
+                    .length;
+                if (_length < 2) {
+                    opacity = ".0" + opacity;
+                } else if (_length === 3) {
+                    opacity = ui.value;
+                } else {
+                    opacity = '.' + opacity;
+                }
+
+                $(_var.drag_div)
+                    .css({
+                        'opacity': opacity
+                    });
+                _var.urlOpacity = opacity;
+
+                $(".opacity__input").val(ui.value);
+      }
+    });
+
+           
+    }
+
+       return {
+
+            initialize : function () {
+                this.setUpListeners();
+            },
+
+            setUpListeners : function () {
+                $('.settings__btn_reset').on('click', _resetAll);
+            }
+        };
+
+}());
+
+resetModul.initialize();
